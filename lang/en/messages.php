8393e3a349eb63018b359cf052663ab1a3eef663
David Goldero <davidgoldero@gmail.com>
2023-04-05 17:40:01 +0200

    fuera lang

--- lang/en/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-return [
-    'welcome'   => 'it is being develop right now, currently we are working on it etc....',
-
-];
--- lang/es/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-return [
-    'welcome'   => 'Estamos trabajando ahora mismo en esta página tan guay',
-
-];
